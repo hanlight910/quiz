bdbf704dd96bf7607f618f69c4d9610324512fd9
hanlight910 <leehanvit511@gmail.com>
2024-10-20 00:38:21 +0900

    feat[algorithm]: give up json file json data directly located in js

--- src/quiz.js
+++ src/quiz.js
@@ -80,12 +80,64 @@ function generateQuiz(questionsData) {
     });
 }
 
-fetch('/quiz/test/questions.json')
-    .then(response => response.json())
-    .then(data => {
-        generateQuiz(data);
-    })
-    .catch(error => {
-		alert("Error laoding the quiz data:", error);
-        console.error('Error loading the quiz data:', error);
-    });
+data = {
+	"questionsData": [
+		{
+			"question": "What is the capital of France?",
+			"options": [
+				"Berlin",
+				"Madrid",
+				"Paris",
+				"Rome"
+			]
+		},
+		{
+			"question": "Which language is primarily used for web development?",
+			"options": [
+				"Python",
+				"JavaScript",
+				"C++",
+				"Ruby"
+			]
+		},
+		{
+			"question": "What is the result of 5 + 3?",
+			"options": [
+				"6",
+				"7",
+				"8",
+				"9"
+			]
+		},
+		{
+			"question": "Which of these animals is a mammal?",
+			"options": [
+				"Shark",
+				"Dolphin",
+				"Lizard",
+				"Frog"
+			]
+		},
+		{
+			"question": "Which of the following is a planet?",
+			"options": [
+				"Pluto",
+				"Venus",
+				"Moon",
+				"Sun"
+			]
+		}
+	]
+}
+
+generateQuiz(data);
+
+// fetch('/quiz/test/questions.json')
+//     .then(response => response.json())
+//     .then(data => {
+//         generateQuiz(data);
+//     })
+//     .catch(error => {
+// 		alert("Error laoding the quiz data:", error);
+//         console.error('Error loading the quiz data:', error);
+//     });
