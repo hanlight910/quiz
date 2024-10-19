7809250d0428c20ba7774dfbc0e816ceff399865
hanlight910 <leehanvit511@gmail.com>
2024-10-20 00:36:05 +0900

    feat[frontend]: modify to root director

--- src/quiz.js
+++ src/quiz.js
@@ -80,7 +80,7 @@ function generateQuiz(questionsData) {
     });
 }
 
-fetch('test/questions.json')
+fetch('/quiz/test/questions.json')
     .then(response => response.json())
     .then(data => {
         generateQuiz(data);
